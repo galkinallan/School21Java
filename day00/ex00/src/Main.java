ecb76c2c7926caab740c6359550eab73d920f2d3
GalkinAllan <galkinallan@gmail.com>
2022-08-25 22:40:14 +0300

    ex00

---
+++ day00/ex00/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 479598;
+        int result = 0;
+        while (num != 0) {
+            result += num % 10;
+            num /= 10;
+        }
+        System.out.println(result);
+    }
+}
